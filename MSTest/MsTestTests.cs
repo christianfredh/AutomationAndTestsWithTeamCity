fb6edb36dec51aaf50833c827140c07a1f69d794
Christian Fredh <christian.fredh@gmail.com>
2016-06-19 17:40:58 +0200

    Added MSTest

---
+++ MSTest/MsTestTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTest
+{
+    [TestClass]
+    public class MsTestTests
+    {
+        [TestMethod]
+        public void ExampleTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
