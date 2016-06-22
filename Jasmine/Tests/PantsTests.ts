381da666428ead70ec3c2a24acc1ae0e8a659383
Christian Fredh <christian.fredh@gmail.com>
2016-06-22 19:41:10 +0200

    Add jasmine tests

---
+++ Jasmine/Tests/PantsTests.ts
@@ -0,0 +1,15 @@
+﻿/// <reference path="../ToTest/Pants.ts" />
+
+describe("Pants", () => {
+
+    var sut = null;
+
+    beforeEach(() => sut = new PantsApp.Pants());
+
+    it("is always in sunshine orange color", () => {
+
+        let color = sut.color;
+
+        expect(color).toBe("sunshineorange");
+    });
+});
---
+++ Jasmine/ToTest/Pants.ts
@@ -0,0 +1,11 @@
+﻿module PantsApp {
+    export class Pants {
+
+        public color: string;
+
+        constructor() {
+            this.color = "sunshineorange";
+        }
+
+    }
+}
