4b3871704d4a6fc8e423d73a439b21eedd71a689
Christian Fredh <christian.fredh@gmail.com>
2016-06-22 21:45:37 +0200

    Update PhantomJS paths

--- CasperJs/CasperJS/batchbin/casperjs.bat
+++ CasperJs/CasperJS/batchbin/casperjs.bat
@@ -2,4 +2,4 @@
 set CASPER_PATH=%~dp0..
 set CASPER_BIN=%CASPER_PATH%\bin\
 set ARGV=%*
-call CASPER_PATH\..\..\packages\PhantomJS.2.1.1\tools\phantomjs\phantomjs "%CASPER_BIN%bootstrap.js" --casper-path="%CASPER_PATH%" --cli %ARGV%
+call CASPER_PATH\..\PhantomJS\phantomjs "%CASPER_BIN%bootstrap.js" --casper-path="%CASPER_PATH%" --cli %ARGV%
