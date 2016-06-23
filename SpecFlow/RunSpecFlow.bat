f414a931e515f3db332d2c999de291f30adc1489
Christian Fredh <christian.fredh@gmail.com>
2016-06-23 10:12:29 +0200

    Add script for running specflow

---
+++ SpecFlow/RunSpecFlow.bat
@@ -0,0 +1,16 @@
+﻿
+
+
+
+"..\packages\NUnit.Console.3.0.0\tools\nunit3-console.exe" /out=TestResult.txt /result=TestResult.xml /framework=net-4.5 bin\Release\SpecFlow.dll
+ 
+"..\packages\SpecFlow.2.1.0\tools\specflow.exe" nunitexecutionreport Specflow.csproj
+ 
+IF NOT EXIST TestResult.xml GOTO FAIL
+IF NOT EXIST TestResult.html GOTO FAIL
+EXIT
+ 
+:FAIL
+echo ##teamcity[buildStatus status='FAILURE']
+EXIT /B 1
+
