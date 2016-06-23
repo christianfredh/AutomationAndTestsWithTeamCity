92aaa55ccd0bba701d1f35857027f2ec704bfa05
Christian Fredh <christian.fredh@gmail.com>
2016-06-23 10:17:08 +0200

    Always overwrite video

--- CasperJs/ffmpeg/MakeVideo.bat
+++ CasperJs/ffmpeg/MakeVideo.bat
@@ -1,1 +1,1 @@
-ffmpeg -start_number 10 -framerate 2 -i dragon%%03d.png -c:v libx264 -r 25 -pix_fmt yuv420p out.mp4
+ffmpeg -y -start_number 10 -framerate 2 -i dragon%%03d.png -c:v libx264 -r 25 -pix_fmt yuv420p out.mp4
