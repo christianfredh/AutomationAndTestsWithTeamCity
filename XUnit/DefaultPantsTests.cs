3f053a21953820c6782022c252ff7d1e90cccac0
Christian Fredh <christian.fredh@gmail.com>
2016-06-23 12:48:53 +0200

    Better pants tests for xunit and fluent assertions

---
+++ XUnit/DefaultPantsTests.cs
@@ -0,0 +1,62 @@
+﻿namespace XUnit
+{
+    using System.Collections.Generic;
+    using FluentAssertions;
+    using Xunit;
+    
+    public class DefaultPantsTests
+    {
+        [Fact]
+        public void ColorIsPurple()
+        {
+            var pants = new Pants();
+
+            pants.Color.Should().Be("purple");
+        }
+
+        [Fact]
+        public void TwoPockets()
+        {
+            var pants = new Pants();
+
+            pants.Pockets.Should().HaveCount(2);
+        }
+
+        [Fact]
+        public void EmptyPockets()
+        {
+            var pants = new Pants();
+
+            pants.Pockets.Should().OnlyContain(pocket => pocket.IsEmpty);
+        }
+    }
+
+
+    public class Pants
+    {
+        public Pants()
+        {
+            Color = "purple";
+            Pockets = new List<PantsPocket>
+            {
+                new PantsPocket(),
+                new PantsPocket(),
+            };
+        }
+
+        public string Color { get; set; }
+
+        public IEnumerable<PantsPocket> Pockets { get; set; }
+
+    }
+
+    public class PantsPocket
+    {
+        public PantsPocket()
+        {
+            IsEmpty = true;
+        }
+
+        public bool IsEmpty { get; set; }
+    }
+}
